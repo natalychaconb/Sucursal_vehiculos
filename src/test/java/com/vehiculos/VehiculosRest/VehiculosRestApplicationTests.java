0aa6ef16b51773a18ae10a49b3081862951d157b
natalychaconb <natalychaconb@gmail.com>
2025-02-17 18:20:14 -0300

    Update VehiculosRestApplicationTests.java

--- src/test/java/com/vehiculos/VehiculosRest/VehiculosRestApplicationTests.java
+++ src/test/java/com/vehiculos/VehiculosRest/VehiculosRestApplicationTests.java
@@ -3,10 +3,10 @@ package com.vehiculos.VehiculosRest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class VehiculosRestApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
